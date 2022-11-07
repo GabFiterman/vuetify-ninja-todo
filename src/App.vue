33a881348ce82a8bf0e00c191bd6e1834a5c5774
GabFiterman <gfiterman96@gmail.com>
2022-11-07 18:18:06 -0300

    Testing Text and Colors

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <v-app>
+
     <v-main>
       <router-view/>
     </v-main>
+    
   </v-app>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <HelloWorld />
+  <v-container class="home">
+    <h1>Home</h1>
+    <p class="bg-red">Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
+    <p class="bg-deep-purple-lighten-5 font-weight-bold text-purple-darken-4">Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
+    <br />
+    <h1 class="text-h1 font-weight-bold">Macaquinho Bonitinho</h1>
+    <h1 class="text-h2">Secondary Display</h1>
+    <h1 class="text-h4">Small Display</h1>
+    <p class="text-headline">This is a headline</p>
+    <p class="text-subheading">This is subheading</p>
+    <p class="text-caption">This is caption</p>
+  </v-container>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
 
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
+export default {
   name: 'HomeView',
 
-  components: {
-    HelloWorld,
-  },
-});
+};
 </script>
